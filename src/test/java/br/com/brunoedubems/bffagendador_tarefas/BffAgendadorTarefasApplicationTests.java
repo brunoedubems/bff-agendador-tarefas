e7bfc53fa495ead66f4ee24dda8811c26d262d75
brunoedubems <bemds1@aluno.ifnmg.edu.br>
2025-09-21 08:58:39 -0300

    feat: adiciona a estrutura do projeto

---
+++ src/test/java/br/com/brunoedubems/bffagendador_tarefas/BffAgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brunoedubems.bffagendador_tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffAgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
